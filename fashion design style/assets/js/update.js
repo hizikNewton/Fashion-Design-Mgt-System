f233d2472ada3c1ba3994030fae6d5b605b4b934
Isaac <bamideleisaac11@gmail.com>
2019-03-01 02:57:16 +0100

    included update for view

---
+++ fashion design style/assets/js/update.js
@@ -0,0 +1,35 @@
+
+
+var userdata = JSON.parse(localStorage.getItem('userdata'))
+var userUploadImg =[]
+$(document).ready(function() {
+
+$.ajax({
+    type: "GET",
+    url:  "http://localhost:3000/designStyles",
+    dataType: 'json',
+    success: function(res){
+        for (i=0;i<res.length;i++){
+            if(res[i].userId==userdata.id){
+                userUploadImg.push(res[i])
+            }
+
+        }
+        for(i=0;i<userUploadImg.length;i++){
+            var id = 'img'+i
+            var img = userUploadImg[i].img
+            var mydiv = `<div class="card" style="width: 18rem;">
+                            <img src=${img} id= ${id} class="card-img-top" alt="...">
+                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Update
+                            </button>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>`
+            $(imageGallery).prepend(mydiv)
+            
+            
+        }
+    }
+});
+
+});
